4c2e794a42659cb55c9cc5380407596ec29c7c1e
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-05 23:20:26 +0700

    Изменение цвета и текста на тестовой странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <div className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">HELLO WORLD</h1>
+        <h1 className="text-4xl font-bold text-red-600">BYE WORLD</h1>
       </div>  
   );
 }
