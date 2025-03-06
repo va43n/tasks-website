752c304ae71cb4ceee0fb2bdd214401e33f2e76f
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-07 02:10:39 +0700

    Исправление синтаксической ошибки

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
 		const response = await fetch("/api/login", {
 			method: "POST",
 			headers: {"Content-Type": "application/json"},
-			body: JSON:stringify(form),
+			body: JSON.stringify(form),
 		});
 
 		const data = await response.json();
