3d84e4fcd6547561b922e4a4cae34b0d7c243b3d
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-07 02:20:26 +0700

    Исправление синтаксической ошибки

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "newxt/server";
+import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import supabase from "@/lib/supabase";
 
