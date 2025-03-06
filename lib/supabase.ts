af759662f448a6c95807e976c81fea848429a73b
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-07 03:11:57 +0700

    Добавление проверки на null url и key в supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+	throw new Error("Отсутствуют Supabase URL или Supabase KEY.");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
