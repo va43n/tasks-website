8b2da94f10bd22d3ef9cb95e8a574fcc3dc940f9
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-07 13:57:41 +0700

    Исправление ошибки обработчика select объекта на форме регистрации

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
 	});
 	const [error, setError] = useState("");
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setForm({...form, [e.target.name]: e.target.value});
 	};
 
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -15,7 +15,7 @@ export default function RegisterPage() {
 	});
 	const [error, setError] = useState("");
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
 		setForm({...form, [e.target.name]: e.target.value});
 	};
 
