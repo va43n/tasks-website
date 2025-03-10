ba7580e6404f495d7cb743562f2d00532572e2ca
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-11 01:31:47 +0700

    Исправление ошибок в edit/page.tsx в HTML коде, связанные с onChange свойством объектов

--- src/app/profile/[username]/edit/page.tsx
+++ src/app/profile/[username]/edit/page.tsx
@@ -102,16 +102,16 @@ export default function EditProfile() {
 			<h1>Редактирование профиля пользователя {username}</h1>
 
 			<h3>Изменение описания:</h3>
-			<textarea value={bio} onChange{(e) => setBio(e.target.value)} />
+			<textarea value={bio} onChange={(e) => setBio(e.target.value)} />
 			<button onClick={updateBio}>Сохранить</button>
 
 			<h3>Добавление нового задания:</h3>
-			<input value={title} onChange{(e) => setTitle(e.target.value)} placeholder="Название задания" /} />
-			<textarea value={description} onChange{(e) => setDescription(e.target.value)} placeholder="Описание задания" /} />
+			<input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Название задания" /} />
+			<textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Описание задания" /} />
 			<p>Прикрепите картинку:</p>
-			<input type="file" accept="image/*" onChange{(e) => setImage(e.target.files?.[0] || null)} className="input-file" />
+			<input type="file" accept="image/*" onChange={(e) => setImage(e.target.files?.[0] || null)} className="input-file" />
 			<p>Прикрепите файл:</p>
-			<input type="file" onChange{(e) => setFile(e.target.files?.[0] || null)} className="input-file" />
+			<input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)} className="input-file" />
 			<button onClick={addTask}>Добавить задание</button>
 
 			<h3>Удаление задания:</h3>
