7dc635ebfb11db5abf2e9fd882d4ef6744abf4b2
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-10 23:17:22 +0700

    В файле profile/page.tsx Task теперь это интерфейс

--- src/app/profile/[username]/edit/page.tsx
+++ src/app/profile/[username]/edit/page.tsx
@@ -3,7 +3,7 @@
 import {useEffect, useState} from "react";
 import {useParams, useRouter} from "next/navigation";
 
-type Task {
+interface Task {
 	title: string;
 	description: string;
 	fileUrl: string;
