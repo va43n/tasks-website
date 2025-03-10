5f93254624624fd18c01f5bff9827a57737e01d1
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-11 02:37:37 +0700

    Добавление NextRequest в [username]/route.ts

--- src/app/api/profile/[username]/route.ts
+++ src/app/api/profile/[username]/route.ts
@@ -1,4 +1,4 @@
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import supabase from "../../../../../lib/supabase";
 
 export async function GET(req: NextRequest) {
