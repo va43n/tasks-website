36a37edf6c8670972eb809e26c488ae712b6ac52
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-03-30 23:57:47 +0700

    Исправление ошибки, связанной с тем, что в register.tsx в инпутах дважды писалось type

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -45,15 +45,15 @@ export default function RegisterPage() {
 			<h3>Регистрация</h3>
 			<input className="form-box-size form-rounded-box" type="text" name="full_name" placeholder="ФИО" onChange={handleChange} />
 			<input className="form-box-size form-rounded-box" type="text" name="username" placeholder="Логин" onChange={handleChange} />
-			<input type="email" className="form-box-size form-rounded-box" type="text" name="email" placeholder="Электронная почта" onChange={handleChange} />
+			<input type="email" className="form-box-size form-rounded-box" name="email" placeholder="Электронная почта" onChange={handleChange} />
 			<div className="form-select-container form-box-size">
 				<select className="form-select-form form-rounded-box" name="role" onChange={handleChange}>
 					<option value="Пациент">Пациент</option>
 					<option value="Доктор">Доктор</option>
 				</select>
 			</div>
-			<input type="password" className="form-box-size form-rounded-box" type="text" name="password" placeholder="Пароль" onChange={handleChange} />
-			<input type="password" className="form-box-size form-rounded-box" type="text" name="confirmPassword" placeholder="Повторите пароль" onChange={handleChange} />
+			<input type="password" className="form-box-size form-rounded-box" name="password" placeholder="Пароль" onChange={handleChange} />
+			<input type="password" className="form-box-size form-rounded-box" name="confirmPassword" placeholder="Повторите пароль" onChange={handleChange} />
 			{error && <p color="red">{error}</p>}
 			<button className="form-box-size form-rounded-box form-button-form" onClick={handleRegister}>Зарегистрироваться</button>
 		</div>
