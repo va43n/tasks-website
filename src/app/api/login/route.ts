25e4a2a97e206befbbc368b76dfe694a88af3a37
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-04-08 23:17:12 +0700

    Замена id на user_id в соответствии с новым названием столбца в таблице users

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: NextRequest) {
 			return NextResponse.json({error: "Неправильно введен логин или пароль"}, {status: 400});
 		}
 
-		const token = createJWT({id: user.id, username: user.username, role: user.role});
+		const token = createJWT({id: user.user_id, username: user.username, role: user.role});
 
 		const response = NextResponse.json({success: true});
 		response.cookies.set("token", token, {httpOnly: true, secure: true, path: "/"});
