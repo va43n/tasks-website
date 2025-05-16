b85736c0f6b70c1ffeee71aef8ac19270b61ccc4
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-05-17 02:48:02 +0700

    Добавление домена облачного хранилища в конфиг next js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+	images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'asooxovwhivjltdaiozk.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
