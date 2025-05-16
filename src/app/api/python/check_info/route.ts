f808d8381cb20c0b5af4f18037795fa90fa44781
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2025-05-17 01:58:36 +0700

    Изменены сообщения, посылаемые Коврику во время проверки логина

--- src/app/api/python/check_info/route.ts
+++ src/app/api/python/check_info/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
 
 	if (!username || !password) {
 		console.log("Не удалось получить username или password");
-		return NextResponse.json({error: "Не удалось получить username или password"}, {status: 400});
+		return NextResponse.json({message: "wrong"}, {status: 400});
 	}
 
 	const {data: user, error: userError} = await supabase
@@ -21,12 +21,12 @@ export async function POST(req: NextRequest) {
 	console.log(user);
 
 	if (!user) {
-		return NextResponse.json({error: "Пользователя " + username + " " + password + "не существует"}, {status: 500});
+		return NextResponse.json({message: "wrong"}, {status: 500});
 	}
 
 	const passwordMatch = await bcrypt.compare(password, user.password);
 	if (!passwordMatch) {
-		return NextResponse.json({error: "Неправильно введен логин или пароль"}, {status: 400});
+		return NextResponse.json({message: "wrong"}, {status: 400});
 	}
 
 	const {data: patient, error: patientError} = await supabase
@@ -38,8 +38,8 @@ export async function POST(req: NextRequest) {
 		console.log(patient);
 
 	if (!patient) {
-		return NextResponse.json({error: "Такого пациента не существует"}, {status: 500});
+		return NextResponse.json({message: "wrong"}, {status: 500});
 	}
 
-	return NextResponse.json({message: "Такой пациент есть!"}, {status: 200});
+	return NextResponse.json({message: "correct"}, {status: 200});
 }
